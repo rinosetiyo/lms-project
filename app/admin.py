337e95c3ade6c045914b0ea01f39d3104d744c4f
rino setiyo <rino.setiyo96@gmail.com>
2023-10-03 13:25:52 +0700

    remodels database

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from app.models import Kelas, Materi, Tugas, Pembayaran
+from app.models import Kelas, Materi, Tugas, Pembayaran, Pelajaran, Jadwal
 
 # Register your models here.
 admin.site.register(Kelas)
 admin.site.register(Materi)
 admin.site.register(Tugas)
 admin.site.register(Pembayaran)
+admin.site.register(Pelajaran)
+admin.site.register(Jadwal)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,10 +11,18 @@ class Kelas(models.Model):
     def __str__(self):
         return self.nama_kelas
     
+class Pelajaran(models.Model):
+    judul_pelajaran = models.CharField(max_length=250)
+    deskripsi = models.TextField()
+    kelas = models.ForeignKey(Kelas, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.judul_pelajaran
+
 class Materi(models.Model):
     judul_materi = models.CharField(max_length=200)
     deskripsi = models.TextField()
-    kelas = models.ForeignKey(Kelas, on_delete=models.CASCADE)
+    pelajaran = models.ForeignKey(Pelajaran, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.judul_materi
@@ -33,6 +42,16 @@ class Tugas(models.Model):
 
     def __str__(self):
         return self.judul_tugas
+
+class Jadwal(models.Model):
+    hari = models.DateField()
+    jam = models.TimeField()
+    jadwal_tambahan = models.CharField(max_length=250)
+    Pelajaran = models.ForeignKey(Pelajaran, on_delete=models.CASCADE)
+    kelas = models.ForeignKey(Kelas, on_delete=models.CASCADE)
+    materi = models.ForeignKey(Materi, on_delete=models.CASCADE)
+    tugas = models.ForeignKey(Tugas, on_delete=models.CASCADE)
+    # nama pengajar
     
 class Pembayaran(models.Model):
     jumlah_pembayaran = models.IntegerField(null=True, blank=True)
